c0bdbbc11d5ba5868130ec38496e4fecfc707f04
Diogo Freitas <diogo.freitas@hotmail.com>
2021-04-24 13:41:23 -0300

    Authorize (JWT)

---
+++ curso.api/Controllers/CursoController.cs
@@ -0,0 +1,56 @@
+﻿using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/cursos")]
+    [ApiController]
+    [Authorize]
+    public class CursoController : ControllerBase
+    {
+        /// <summary>
+        /// Este serviço permite cadastrar curso para o usuário autenticado.
+        /// </summary>
+        /// <param name="cursoViewModelInput"></param>
+        /// <returns>Retorna status 201 e dados do curso do usuário</returns>
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao cadastrar um curso", Type = typeof(CursoViewModelInput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+        /// <summary>
+        /// Este serviço permite obter todos os cursos ativos do usuário.
+        /// </summary>
+        /// <returns>Retorna status ok e dados do curso do usuário</returns>
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao obter os cursos", Type = typeof(List<CursoViewModelInput>))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            //var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            var cursos = new List<CursoViewModelOutput>();
+
+            cursos.Add(new CursoViewModelOutput
+            {
+                Login = "1",
+                Descricao = "teste",
+                Nome = "teste"
+            });
+
+            return Ok(cursos);
+        }
+    }
+}
--- curso.api/Controllers/UsuarioController.cs
+++ curso.api/Controllers/UsuarioController.cs
@@ -3,10 +3,14 @@ using curso.api.Models;
 using curso.api.Models.Usuarios;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace curso.api.Controllers
@@ -15,11 +19,11 @@ namespace curso.api.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        //<summary>
-        //Este serviço permite autenticar um usuario cadastrado e ativo
-        //</summary>
-        //<param name="loginViewModelInput">View model do login</param>
-        //<returns>Retorna status ok, dados do usuario e o token em caso de sucesso</returns>
+        ///<summary>
+        ///Este serviço permite autenticar um usuario cadastrado e ativo
+        ///</summary>
+        ///<param name="loginViewModelInput">View model do login</param>
+        ///<returns>Retorna status ok, dados do usuario e o token em caso de sucesso</returns>
         [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
         [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
         [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
@@ -32,7 +36,36 @@ namespace curso.api.Controllers
             //{
             //    return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
             //}
-            return Ok(loginViewModelInput);
+
+            var usuarioViewModelOutput = new UsuarioViewModelOutput()
+            {
+                Codigo = 1,
+                Login = "Diogo",
+                Email = "diogo@teste.com"
+            };
+
+            var secret = Encoding.ASCII.GetBytes("MzfsT&d9gprP>!9$Es(X!5g@;ef!5sbk:jH\\2.}8ZP'qY#7");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Login.ToString()),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenerated);
+
+            return Ok(new
+            {
+                Token = token,
+                Usuario = usuarioViewModelOutput
+            });
         }
         [HttpPost]
         [Route("registrar")]
